2d336c062482c522d4c34c9d76df05dc144099e9
L Vamsi Krishna <53250258+l-vamsi-krishna@users.noreply.github.com>
2022-09-14 12:36:06 +0530

    Create 02. String Split and Join.py

---
+++ Python/03. Strings/02. String Split and Join.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/python-string-split-and-join/problem
+# Score = 10
+def split_and_join(line):
+    # write your code here
+    line = line.split()
+    line = '-'.join(line)
+    return line
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
